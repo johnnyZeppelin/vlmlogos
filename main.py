beb836c041c5d5dd6df7e3953134eb6f1dee975b
sifanli <johnsifanli@gmail.com>
2025-08-11 07:52:41 +0000

    Maybe it should be this.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from transformers import AutoProcessor, AutoModelForVision2Seq
 ## added
 from PIL import Image
 import torch
+import os
 ## end
 
 # processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-72B-Instruct")
@@ -12,28 +13,37 @@ processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
 model = AutoModelForVision2Seq.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
 
 ## added
+img_dir = './img'
+
+with os.scandir(img_dir) as entries:
+    filenames = [entry.name for entry in entries if entry.is_file()]
+
+print(filenames)
 # Load local image
-image_path = "/path/to/your/image.jpg"
+image_path = os.path.join(img_dir, filenames[0])
 local_image = Image.open(image_path).convert("RGB")
 ## end
 
-messages = [
-    {
-        "role": "user",
-        "content": [
-            # {"type": "image", "url": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/p-blog/candy.JPG"},
-            {"type": "image", "image": local_iamge},
-            {"type": "text", "text": "What text do you see from this image?"}
-        ]
-    },
-]
-inputs = processor.apply_chat_template(
-	messages,
-	add_generation_prompt=True,
-	tokenize=True,
-	return_dict=True,
-	return_tensors="pt",
-).to(model.device)
+def use_model(image, text, processor, model):
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                # {"type": "image", "url": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/p-blog/candy.JPG"},
+                {"type": "image", "image": iamge},
+                {"type": "text", "text": text}
+            ]
+        },
+    ]
+    inputs = processor.apply_chat_template(
+    	messages,
+    	add_generation_prompt=True,
+    	tokenize=True,
+    	return_dict=True,
+    	return_tensors="pt",
+    ).to(model.device)
+    outputs = model.generate(**inputs, max_new_tokens=40)
+    return processor.decode(outputs[0][inputs["input_ids"].shape[-1]:])
 
 ## updated but questioned, so it's commented out
 # Generate
@@ -43,5 +53,13 @@ inputs = processor.apply_chat_template(
 # print(processor.decode(outputs[0][inputs["input_ids"].shape[-1]:], skip_special_tokens=True))
 ##
 
-outputs = model.generate(**inputs, max_new_tokens=40)
-print(processor.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
+# outputs = model.generate(**inputs, max_new_tokens=40)
+# print(processor.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
+
+for i in range(1):
+    image_path = os.path.join(img_dir, filenames[i])
+    local_image = Image.open(image_path).convert("RGB")
+    text = "What text do you see from this image?"
+    use_model(local_image, text, processor, model)
+    print('=================')
+
